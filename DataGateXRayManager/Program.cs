dbd878c4c462fde9511d4dce08801dbba49bf7a8
Ivan <imkolganov@gmail.com>
2026-08-08 15:47:50 +0300

    Report node PublicIp on /api/info via ExternalIpAddressService.
    Resolve WAN IP with Accept text/plain and ifconfig.me/ip, include PublicIp in RootXrayInfoResponse, and bump to 1.1.2.7 with SharedModels 1.0.48.

--- DataGateXRayManager/Program.cs
+++ DataGateXRayManager/Program.cs
@@ -3,6 +3,7 @@ using DataGateXRayManager.Configurations;
 var builder = WebApplication.CreateBuilder(args);
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 builder.Host.ConfigureSerilog();
+builder.ConfigureExternalIpServices();
 
 builder.Services.ConfigureServices(builder.Configuration);
 builder.Services.ConfigureSignalR(builder.Configuration);
